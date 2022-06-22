2e42dba312ecc15dd9cd9e69632802cf0803268c
Shivani3105 <guptashivani312002@ms.du.ac.in>
2022-06-22 16:46:22 +0530

    food recipe first

---
+++ lib/model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+
+class RecipeModel
+{
+  late String applabel;
+  late String appimgUrl;
+  late double appcalories;
+  late String appurl;
+
+  RecipeModel({this.applabel = "LABEL",this.appcalories = 0.000 ,this.appimgUrl = "IMAGE",this.appurl = "URL"});
+  factory RecipeModel.fromMap(Map recipe)
+  {
+    return RecipeModel(
+      applabel: recipe["label"],
+      appcalories: recipe["calories"],
+      appimgUrl: recipe["image"],
+      appurl: recipe["url"]
+    );
+  }
+} 
