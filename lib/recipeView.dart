3d401e39bc7dfd8b4fa499e87645376e7ca27255
Shivani Gupta <guptashivani312002@gmail.com>
2025-07-05 08:51:53 +0530

    Updated RecipeView and pubspec.yaml

--- lib/recipeView.dart
+++ lib/recipeView.dart
@@ -1,46 +1,60 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/foundation.dart'; // for kIsWeb
+import 'package:url_launcher/url_launcher.dart'; // for opening link on web
+import 'package:webview_flutter/webview_flutter.dart'; // for mobile platforms
+
 class RecipeView extends StatefulWidget {
-  String url;
-  RecipeView(this.url);
+  final String url;
+
+  const RecipeView(this.url, {super.key});
 
   @override
-  _RecipeViewState createState() => _RecipeViewState();
+  State<RecipeView> createState() => _RecipeViewState();
 }
 
 class _RecipeViewState extends State<RecipeView> {
   late String finalUrl;
-  final Completer<WebViewController> controller=Completer<WebViewController>();
-  @override
-  void initState(){
-    if(widget.url.toString().contains('http://'))
-    {
-      finalUrl=widget.url.toString().replaceAll('http://','https://');
+  late final WebViewController _controller;
 
+  @override
+  void initState() {
+    super.initState();
+
+    // Convert 'http' to 'https' if needed
+    if (widget.url.contains('http://')) {
+      finalUrl = widget.url.replaceAll('http://', 'https://');
+    } else {
+      finalUrl = widget.url;
     }
-    else{
-      finalUrl=widget.url;
+
+    if (kIsWeb) {
+      // Launch URL in external browser for web
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        launchUrl(
+          Uri.parse(finalUrl),
+          mode: LaunchMode.externalApplication,
+        );
+      });
+    } else {
+      // Setup WebViewController for mobile
+      _controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..loadRequest(Uri.parse(finalUrl));
+    }
   }
-  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Food Recipe App'),
+        title: const Text('Flutter Food Recipe App'),
       ),
-      body: Container(
-        child: WebView(
-          initialUrl:finalUrl,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController){
-            setState(() {
-              controller.complete(webViewController);
-            });
-          }
-
-        ),
-      ),
+      body: kIsWeb
+          ? const Center(
+              child: Text("Opening recipe in a new tab..."),
+            )
+          : WebViewWidget(controller: _controller),
     );
   }
 }
